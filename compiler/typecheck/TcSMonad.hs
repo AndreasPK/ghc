428f4f2f2563908b531374aed7016a222ad04a7f
Joachim Breitner <mail@joachim-breitner.de>
2018-01-21 10:01:50 -0500

    use tcLookupGlobal in TcS’s MonadThings instance

--- compiler/typecheck/TcSMonad.hs
+++ compiler/typecheck/TcSMonad.hs
@@ -128,7 +128,8 @@ import FamInstEnv
 import qualified TcRnMonad as TcM
 import qualified TcMType as TcM
 import qualified TcEnv as TcM
-       ( checkWellStaged, topIdLvl, tcGetDefaultTys, tcLookupClass, tcLookupId )
+       ( checkWellStaged, topIdLvl, tcGetDefaultTys
+       , tcLookupClass, tcLookupId, tcLookupGlobal )
 import PrelNames( heqTyConKey, eqTyConKey )
 import Kind
 import TcType
@@ -2390,7 +2391,7 @@ instance HasModule TcS where
    getModule = wrapTcS getModule
 
 instance MonadThings TcS where
-   lookupThing n = wrapTcS (lookupThing n)
+   lookupThing n = wrapTcS (TcM.tcLookupGlobal n)
 
 -- Basic functionality
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
