1f749c79e7366957d2984423ccb9dbe2816a217b
klebinger.andreas@gmx.at <klebinger.andreas@gmx.at>
2018-01-29 22:49:21 +0100

    Test case comment, remove unsafeCoerce

--- testsuite/tests/codeGen/should_run/T14619.hs
+++ testsuite/tests/codeGen/should_run/T14619.hs
@@ -1,10 +1,24 @@
 {-# OPTIONS_GHC -O1 #-}
 
+{-
+  On windows some xmm registers are callee saved. This means
+  they can't be used as scratch registers before a call to C.
+
+  In #14619 this wasn't respected which lead to a wrong value
+  ending up in xmm6 and being returned in the final result.
+
+  This code compiles to a non trivial fp computation followed
+  by a call to sqrt at O1+. If xmm6 isn't properly handled it
+  will be used as a scratch register failing the test.
+
+  The original code used regular sqrt which on 8.2 generated
+  a C call in the backend. To imitate this behaviour on 8.4+
+  we force a call to a C function instead.
+-}
+
 module Main (main) where
 
---The original code used regular sqrt which on 8.2 generated a c call
---behind the scenes. The immitate this behaviour on 8.4+ we force a
---call to a C function instead.
+
 
 import Prelude hiding((*>), (<*))
 import Foreign.C
@@ -26,7 +40,7 @@ sphereIntersection orig dir@(_, _, dirz)
   | t1   < 0  = orig
   | otherwise = undefined
     where b  = orig `absf` dir
-          sqrtDisc = unsafeCoerce call_sqrt $ CDouble b
+          sqrtDisc = realToFrac . call_sqrt $ CDouble b
           t1 = b - sqrtDisc
 
 main = print $ sphereIntersection (11, 22, 33) (44, 55, 66)
